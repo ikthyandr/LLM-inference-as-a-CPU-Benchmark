1de777590d2c11b6442f0cb3dcc889b89ab5a128
ikthyandr <arnab@arm64.cloud>
2025-01-14 23:31:46 +0600

    spelling corrected on config

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ DEFAULT_MODEL = "llama3.2:3b"
 KEY_FILE = '<Name-of-the-key-file>' # Add the path to the key file
 INVENTORY_FILE = 'inventory.yaml'
 PROMPT = "Explain quantum computing in detail."
-NUM_OF_ITERATIONS = 3 # How rigotous the inference should be
+NUM_OF_ITERATIONS = 3 # How rigorous the inference should be
 OUTPUT_DIR = 'benchmark_visuals'
